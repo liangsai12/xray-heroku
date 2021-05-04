4d18901a366e675e31139729965a359bfd1ab149
xkh01 <xingkaihao0917@gmail.com>
2021-05-04 15:54:29 +0800

    Create app.json

---
+++ run.sh
@@ -0,0 +1,25 @@
+cat <<EOF>/etc/xray/config.json
+{
+	"inbounds":[{
+		"port":$PORT,
+		"listen":"0.0.0.0",
+		"protocol":"vless",
+		"settings":{
+			"clients":[{
+				"id":"$UUID"
+			}],
+			"decryption":"none"
+		},
+		"streamSettings":{
+			"network":"ws",
+			"wsSettings":{
+				"path":"$WSPATH"
+			}
+		}
+	}],
+	"outbounds":[{
+		"protocol":"freedom"
+	}]
+}
+EOF
+/usr/bin/xray run --config=/etc/xray/config.json
